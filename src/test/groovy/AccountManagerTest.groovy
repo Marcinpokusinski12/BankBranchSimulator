302a676a4315eb7d40cec07bbffb7483caf2d287
marcinpokusinski12 <imp3rial89@gmail.com>
2022-07-02 23:18:31 +0200

    Transfer Test 13

--- src/test/groovy/AccountManagerTest.groovy
+++ src/test/groovy/AccountManagerTest.groovy
@@ -18,6 +18,20 @@ class AccountManagerTest extends Specification{
         accountManager.transactionList(userFirst).stream().count() == 2
     }
 
+    def "Check the negative amount of money transfer"() {
+        given:
+        def bank = Bank.createNewBank("Bank", "glowny", "email")
+        def accountManager = new AccountManager();
+        def userFirst = new AccountManager().createUserAccount("Test", 100.0,"123123123", bank);
+        def userSecond = new AccountManager().createUserAccount("Test", 100.0,"123123123", bank);
+
+        when:
+        boolean check = accountManager.transferMoney(userFirst, userSecond, -50.00)
+
+        then:
+        !check
+    }
+
     def "Add Account to existing Bank"() {
         given:
         def bank = Bank.createNewBank("Bank", "glowny", "email")
