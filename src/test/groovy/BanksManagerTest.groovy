48cfb68630a186596e4e9151736c2f5dbc33282c
marcinpokusinski12 <imp3rial89@gmail.com>
2022-07-02 23:07:41 +0200

    BankManager Test 6

--- src/test/groovy/BanksManagerTest.groovy
+++ src/test/groovy/BanksManagerTest.groovy
@@ -1,8 +1,23 @@
+import bankbranch.Bank
 import bankbranch.BankBranchMain
 import bankbranch.BanksManager
 import spock.lang.Specification
 
 class BanksManagerTest extends Specification{
+
+    def "Test method add bank to branch"() {
+        given:
+        def bankBranch = BankBranchMain.createBankBranch("Test branch")
+        def bank = Bank.createNewBank("Test", "test2", "test3")
+        def manager = new BanksManager();
+
+        when:
+        manager.addBranch(bankBranch)
+        def isValid = manager.addBankToBranch(bank, bankBranch);
+
+        then:
+        isValid
+    }
 
     def "Add Branch to collection"() {
         given:
