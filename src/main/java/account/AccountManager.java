ab41e40118457c2e18ec1bbcfdef1a5b3d187ea6
marcinpokusinski12 <imp3rial89@gmail.com>
2022-07-02 23:12:06 +0200

    Create AccountManager.java

---
+++ src/main/java/account/AccountManager.java
@@ -0,0 +1,37 @@
+package account;
+
+import bankbranch.Bank;
+import exceptions.BalanceCannotBeNegativeException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountManager {
+    public static List<Account> AVAILABLE_USER_ACCOUNTS = new ArrayList<>();
+
+    public Account createUserAccount(String accountOwnerBank, double balance, String accountNumber, Bank bank) throws BalanceCannotBeNegativeException {
+        var user = new Account(accountOwnerBank, balance, accountNumber, bank);
+        AVAILABLE_USER_ACCOUNTS.add(user);
+        return user;
+    }
+
+    public void depositMoneyToAccount(Account user, double depositedAmount) {
+        if (depositedAmount > 0) {
+            changeBalance(user, depositedAmount);
+        } else {
+            System.out.println("Deposited amount is zero or less than zero");
+        }
+    }
+
+    private void changeBalance(Account userAccount, double depositedAmount) {
+        double balance = userAccount.getBalance();
+        userAccount.setBalance(depositedAmount + balance);
+        String message = "Amount deposited " + depositedAmount + "balance " + userAccount.getBalance();
+    }
+
+}
+
+
+
+
+
