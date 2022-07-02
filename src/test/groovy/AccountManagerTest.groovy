6651456a590bcf2298fa88afbc34b5e09116219d
marcinpokusinski12 <imp3rial89@gmail.com>
2022-07-02 23:12:00 +0200

    AccountManager Test 8

--- src/test/groovy/AccountManagerTest.groovy
+++ src/test/groovy/AccountManagerTest.groovy
@@ -16,5 +16,17 @@ class AccountManagerTest extends Specification{
         bank.getUserAccounts().forEach(x -> x.accountOwner == "Test")
     }
 
+    def "Deposit money to account"() {
+        given:
+        def account = new AccountManager().createUserAccount("Test", 100.0, "123123123", Bank.createNewBank("Bank", "glowny","email"))
+        def accountManager = new AccountManager()
+
+        when:
+        accountManager.depositMoneyToAccount(account, 100);
+
+        then:
+        account.getBalance() == 200;
+    }
+
 
 }
