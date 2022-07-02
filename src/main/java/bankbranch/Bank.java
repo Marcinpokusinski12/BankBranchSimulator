6cbf4ceeedf12738306de95a8abed41dfe0f1a09
marcinpokusinski12 <imp3rial89@gmail.com>
2022-07-02 23:01:45 +0200

    Create Bank.java

---
+++ src/main/java/bankbranch/Bank.java
@@ -0,0 +1,46 @@
+package bankbranch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    public static List<Bank> AVAILABLE_BANKS = new ArrayList<>();
+    private static int nextId = 1;
+    private int id;
+    private String bankName;
+    private String bankAddress;
+    private String contact;
+
+    public Bank(String bankName, String bankAddress, String contact) {
+        this.id = nextId++;
+        this.bankName = bankName;
+        this.bankAddress = bankAddress;
+        this.contact = contact;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+
+    public String getBankAddress() {
+        return bankAddress;
+    }
+
+
+    public String getContact() {
+        return contact;
+    }
+
+
+    public static Bank createNewBank(String bankName, String bankAddress, String contact) {
+        var bank = new Bank(bankName, bankAddress, contact);
+        AVAILABLE_BANKS.add(bank);
+        return bank;
+    }
+}
